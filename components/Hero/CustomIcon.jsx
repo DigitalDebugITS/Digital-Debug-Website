5c8ce55de5286d15d2f9b186674eb7b366e9beb4
Ernest Lubinda <ernestlubinda1@gmail.com>
2023-12-19 18:29:16 +0200

    icon hero

---
+++ components/Hero/CustomIcon.jsx
@@ -0,0 +1,8 @@
+// components/CustomIcon.js
+import { DeviconJavascript } from 'devicon/react';
+
+const JavascriptIcon = () => {
+  return <DeviconJavascript size={64} />;
+};
+
+export { JavascriptIcon };
---
+++ components/icons/ReactIcon.tsx
@@ -0,0 +1,14 @@
+// icons/ReactIcon.js
+
+import 'devicon/react';
+
+const ReactIcon = () => {
+  return (
+    <div>
+      {/* Use the React icon here */}
+      <i className="devicon-react-original"></i>
+    </div>
+  );
+};
+
+export default ReactIcon;
